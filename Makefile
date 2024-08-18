376a379b0c8582bc6d187727df666e0a039dbde2
Daniele Perot <daniele.perot@nordhealth.com>
2024-08-18 12:27:18 +0300

    Link and run with Raylib

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
+CFLAGS= -Wall -Wpedantic \
+		--std=c11 \
+		-L lib/raylib/lib -lraylib -I lib/raylib/include
+
 yarl:
-	gcc src/main.c --std=c11 -o build/yarl
+	gcc src/main.c $(CFLAGS) -o build/yarl
 
 run: yarl
 	./build/yarl
--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include "raylib.h"
 
 #define GRID_SIZE 40
 
 int main() {
-    for (int y = 0; y < GRID_SIZE; y++) {
-        for (int x = 0; x < GRID_SIZE; x++) {
-            printf("##");
-        }
-
-        printf("\n");
+  for (int y = 0; y < GRID_SIZE; y++) {
+    for (int x = 0; x < GRID_SIZE; x++) {
+      printf("##");
     }
 
-    return 0;
+    printf("\n");
+  }
+
+  return 0;
 }
