4a256803d396883ac33b925533434dcde05476e2
Daniele Perot <daniele.perot@nordhealth.com>
2024-08-18 13:03:37 +0300

    Refactor using vec2

--- src/main.c
+++ src/main.c
@@ -11,24 +11,25 @@ typedef enum {
 } Direction;
 
 typedef struct {
-    int scene_x;
-    int scene_y;
+    Vector2 pos;
     Direction direction;
 } Player;
 
 void player_draw(Player *player) {
-    DrawRectangle(player->scene_x, player->scene_y, PLAYER_SIZE, PLAYER_SIZE, RED);
-    DrawCircle(player->scene_x + PLAYER_SIZE / 4, player->scene_y + PLAYER_SIZE / 4, PLAYER_SIZE / 6, WHITE);
-    DrawCircle(player->scene_x + PLAYER_SIZE / 4, player->scene_y + PLAYER_SIZE / 4, PLAYER_SIZE / 12, BLACK);
-    DrawCircle(player->scene_x + PLAYER_SIZE / 4 * 3, player->scene_y + PLAYER_SIZE / 4, PLAYER_SIZE / 6, WHITE);
-    DrawCircle(player->scene_x + PLAYER_SIZE / 4 * 3, player->scene_y + PLAYER_SIZE / 4, PLAYER_SIZE / 12, BLACK);
+    DrawRectangle(player->pos.x, player->pos.y, PLAYER_SIZE, PLAYER_SIZE, RED);
+    DrawCircle(player->pos.x + PLAYER_SIZE / 4, player->pos.y + PLAYER_SIZE / 4, PLAYER_SIZE / 6, WHITE);
+    DrawCircle(player->pos.x + PLAYER_SIZE / 4, player->pos.y + PLAYER_SIZE / 4, PLAYER_SIZE / 12, BLACK);
+    DrawCircle(player->pos.x + PLAYER_SIZE / 4 * 3, player->pos.y + PLAYER_SIZE / 4, PLAYER_SIZE / 6, WHITE);
+    DrawCircle(player->pos.x + PLAYER_SIZE / 4 * 3, player->pos.y + PLAYER_SIZE / 4, PLAYER_SIZE / 12, BLACK);
 }
 
 int main() {
     Player player = {
-        .scene_x = 200,
-        .scene_y = 200,
-        .direction = DIR_UP
+        .pos = {
+            .x = 200,
+            .y = 200,
+        },
+        .direction = DIR_UP,
     };
 
     float dx = 7;
@@ -38,16 +39,6 @@ int main() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        if (player.scene_x <= 0 || player.scene_x + PLAYER_SIZE >= GetScreenWidth()) {
-            dx *= -1;
-        }
-
-        if (player.scene_y <= 0 || player.scene_y + PLAYER_SIZE >= GetScreenHeight()) {
-            dy *= -1;
-        }
-
-        player.scene_x += dx;
-        player.scene_y += dy;
 
         BeginDrawing();
         ClearBackground(GetColor(0x181818FF));
